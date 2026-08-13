ccf8e7730c8ea449bcc969fe8da7dcd4143b2118
Husseinkhdair <hussein.khdair@samaalasima.net>
2026-08-13 11:10:41 +0300

    Refactor main.py to serve static HTML file and add index.html for API welcome page

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
-from datetime import timedelta
-
-from fastapi import Depends, FastAPI
-from Schemas.ProductSchema import ProductSchema
-from functions.get_current_user import get_current_user
-from functions.jwt import create_access_token
-from models.product_model import Product
-from models.user_model import User
-from Schemas.user_register_schema import UserRegisterSchema
-from DTOs.user_info_dto import UserInfoDTO
-from functions.security import hash_password, verify_password
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse
 from controllers.auth_controller import router as auth_router
-
+from pathlib import Path
 app = FastAPI()
 app.include_router(auth_router)
 
+BASE_DIR = Path(__file__).resolve().parent
+HTML_FILE_PATH = BASE_DIR / "static" / "index.html"
 
-
-
-
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/", response_class=FileResponse)
+def root():
+    return FileResponse(HTML_FILE_PATH)
 
 
 
