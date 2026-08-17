bcd61f92e06fa518c91dd153e4dd93cb4d0a6190
Husseinkhdair <hussein.khdair@samaalasima.net>
2026-08-17 10:06:22 +0300

    Implement user registration and login in FackAuthRepository; add dependencies.py and auth_controller.py for FastAPI integration

--- data/auth/data_sources/fack_auth_repository.py
+++ data/auth/data_sources/fack_auth_repository.py
@@ -1,3 +1,5 @@
+from core import Result
+
 from domain.auth.entities.user_entitiy import UserDBEntity
 from domain.auth.repository.auth_repository import AuthRepository
 
@@ -6,11 +8,23 @@ class FackAuthRepository(AuthRepository):
     def __init__(self):
         self.users = {}
 
-    async def register_user(self, user_entity: UserDBEntity):
+    async def register_user(self, user_entity: UserDBEntity)-> Result[UserDBEntity]:
         try:
-            
+            self.users[user_entity.email] = user_entity
+            return Result.success(user_entity)
+        except Exception as e:
+            return Result.failure(str(e))
 
-    async def login_user(self, email: str, password: str):
-        raise NotImplementedError
+    async def login_user(self, email: str, password: str)-> Result[UserDBEntity]:
+        try:
+            user = self.users.get(email)
+            if user is None:
+                return Result.failure("User not found")
+            if user and user.password == password:
+                return Result.success(user)
+            else:
+                return Result.failure("Invalid email or password")
+        except Exception as e:
+            return Result.failure(str(e))
 
 
---
+++ presentation/auth/controller/auth_controller.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter, status
+
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["Auth Controller"]
+)
+
